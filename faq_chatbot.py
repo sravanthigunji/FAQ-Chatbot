86ba59f095f33ec946aad94043dbefc30af7eb4f
sravanthigunji <sravanthigunji9@gmail.com>
2026-05-12 18:33:50 +0530

    FAQ Chatbot project

---
+++ faq_chatbot.py
@@ -0,0 +1,85 @@
+faq_data = {
+    "What payment methods are accepted?":
+    "We accept UPI, credit cards, debit cards and net banking.",
+
+    "How can I track my order?":
+    "You can track your order in the My Orders section.",
+
+    "What is the return policy?":
+    "Products can be returned within 7 days of delivery.",
+
+    "How long does delivery take?":
+    "Delivery usually takes 3 to 5 business days.",
+
+    "Do you offer cash on delivery?":
+    "Yes, cash on delivery is available for selected locations."
+}
+
+import nltk
+import string
+
+from nltk.tokenize import word_tokenize
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+
+def preprocess(text):
+
+    # convert to lowercase
+    text = text.lower()
+
+    # remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # tokenize words
+    words = word_tokenize(text)
+
+    return " ".join(words)
+
+
+questions = list(faq_data.keys())
+
+processed_questions = [preprocess(q) for q in questions]
+
+
+vectorizer = TfidfVectorizer()
+
+X = vectorizer.fit_transform(processed_questions)
+
+
+def chatbot(user_input):
+
+    processed_input = preprocess(user_input)
+
+    user_vector = vectorizer.transform([processed_input])
+
+    similarity = cosine_similarity(user_vector, X)
+
+    best_match_index = similarity.argmax()
+
+    best_score = similarity[0][best_match_index]
+
+    if best_score > 0.3:
+
+        best_question = questions[best_match_index]
+
+        return faq_data[best_question]
+
+    else:
+        return "Sorry, I could not understand your question."
+
+
+while True:
+
+    user = input("You: ")
+
+    if user.lower() == "exit":
+        break
+
+    response = chatbot(user)
+
+    print("Bot:", response)
